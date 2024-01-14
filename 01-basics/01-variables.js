beaf6c3d06af292474676d6ac78a0c6cb0d62ed9
Harshit Kumar <106590879+harshit38144@users.noreply.github.com>
2024-01-14 08:15:41 +0000

    Let const var done

---
+++ 01-basics/01-variables.js
@@ -0,0 +1,10 @@
+const studentId = 123123
+let studentName = "Harshit"
+var studentEmail = "hk@gmail.com"
+studentRoll = 20011007
+
+studentName = 'rvn'
+studentEmail = 'hhh@gmail.com'
+studentRoll = 11111
+
+console.table([studentId, studentName, studentEmail, studentRoll])
