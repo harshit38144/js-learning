fc7265e0c1e2a029e110bd549f9ca47102656f54
Harshit Kumar <106590879+harshit38144@users.noreply.github.com>
2024-01-16 04:46:17 +0000

    call by ref. and value.

--- 01-basics/datatypesSummary.js
+++ 01-basics/datatypesSummary.js
@@ -12,7 +12,7 @@ let userEmail;
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+//console.log(id === anotherId);
 
 // const bigNumber = 3456543576654356754n
 
@@ -29,9 +29,28 @@ let myObj = {
 }
 
 const myFunction = function(){
-    console.log("Hello world");
+    //console.log("Hello world");
 }
 
-console.log(typeof anotherId);
+//console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+let myName1 = "Harsh"
+let myName2 = myName1
+myName2 = "Harshit"
+console.log(myName1) //Harsh
+console.log(myName2) //Harshit
+
+let userOne = {
+    Name: "Harsh",
+    Branch: "CSE",
+    Roll: 20011007
+}
+
+let userTwo = userOne
+//userTwo.Name = "Harshit"
+
+console.log(userOne)
+console.log(userTwo)
