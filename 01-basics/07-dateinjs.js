997b4b7c8e554a0c7257d5e6ee5635a809018fae
Harshit Kumar <106590879+harshit38144@users.noreply.github.com>
2024-03-21 21:01:27 +0000

    date in js

---
+++ 01-basics/07-dateinjs.js
@@ -0,0 +1,7 @@
+let myDate = new Date()
+console.log(myDate.toString()); //Thu Mar 21 2024 20:58:05 GMT+0000 (Coordinated Universal Time)
+console.log(myDate.toDateString());  //Thu Mar 21 2024
+console.log(myDate.toISOString());  //2024-03-21T20:58:05.675Z
+console.log(myDate.toLocaleString());  //3/21/2024, 8:56:29 PM
+console.log(myDate.toTimeString());  //20:56:29 GMT+0000 (Coordinated Universal Time)
+console.log(typeof myDate);  //object
