4327b8a00f80ceede65b9f2d5985a32ddf4efd4e
Harshit Kumar <106590879+harshit38144@users.noreply.github.com>
2024-03-21 11:26:44 +0000

    Strings in Js

---
+++ 01-basics/Strings.js
@@ -0,0 +1,10 @@
+const name = 'kameswari'
+const age = 22
+console.log(name + age)
+console.log(`I'm ${name} and I'm ${age} years old`);
+const gameName = new String('Harshit')
+console.log(gameName.toUpperCase());
+console.log(gameName.length);
+console.log(gameName.toLowerCase())
+console.log(gameName.trim())
+console.log(gameName.indexOf('r'));
