61162ddbe63512002be1393eef752a43926bc39b
Harshit Kumar <106590879+harshit38144@users.noreply.github.com>
2024-03-22 10:42:14 +0000

    objects in js

---
+++ 02-basics/04-objects.js
@@ -0,0 +1,26 @@
+//const tinderUser = new Object() // singleton object
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Rao"
+tinderUser.isLoggedIn = false
+
+//console.log(tinderUser);
+
+const regularUser = {
+    email: "anyone@gmail.com",
+    fullname: {
+        userFullName: {
+            firstName: "Harshit",
+            lastName: "Kumar"
+        }
+    }
+
+}
+//console.log(regularUser.fullname.userFullName.firstName);
+
+const obj1 = {1: "a", 2: "b"}
+
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
