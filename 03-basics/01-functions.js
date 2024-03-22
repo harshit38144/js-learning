daa539af92bc48381e58f0116e85877fd97a3ae7
Harshit Kumar <106590879+harshit38144@users.noreply.github.com>
2024-03-22 15:53:46 +0000

    function in js

---
+++ 03-basics/01-functions.js
@@ -0,0 +1,27 @@
+
+
+// function myName(){
+//     console.log("H");
+//     console.log("A");
+//     console.log("R");
+//     console.log("S");
+//     console.log("H");
+//     console.log("I");
+//     console.log("T");
+// }
+// myName()
+
+function score(player1, player2){
+    let result = player1 + player2
+    return result
+    console.log(player1 + player2);
+}
+// score(5, 3)
+const result = score(5, 5)
+// console.log(result);
+
+function loginUserMessage(username){
+    return `${username} just logged in`
+}
+
+console.log(loginUserMessage("Harsh"))
