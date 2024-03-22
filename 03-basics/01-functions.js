d01b44ede5ac78887898685452a8ba63fc16fe4f
Harshit Kumar <106590879+harshit38144@users.noreply.github.com>
2024-03-22 18:47:43 +0000

    functions

--- 03-basics/01-functions.js
+++ 03-basics/01-functions.js
@@ -11,17 +11,32 @@
 // }
 // myName()
 
-function score(player1, player2){
-    let result = player1 + player2
-    return result
-    console.log(player1 + player2);
-}
-// score(5, 3)
-const result = score(5, 5)
-// console.log(result);
+// function score(player1, player2){
+//     let result = player1 + player2
+//     return result
+//     console.log(player1 + player2);
+// }
+// // score(5, 3)
+// const result = score(5, 5)
+// // console.log(result);
 
-function loginUserMessage(username){
-    return `${username} just logged in`
+// function loginUserMessage(username){
+//     return `${username} just logged in`
+// }
+
+// console.log(loginUserMessage("Harsh"))
+
+function priceCalc(...num1){
+    return num1
+}
+console.log(priceCalc(200, 300, 300));
+
+const user = {
+    username: "Harsh",
+    price: 3
 }
 
-console.log(loginUserMessage("Harsh"))
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+handleObject(user)
