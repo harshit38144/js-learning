2cd5dfb78ff3eb3d533d9498a5d4f648695c970b
Harshit Kumar <106590879+harshit38144@users.noreply.github.com>
2024-03-24 10:51:21 +0000

    break and continue

--- 05-iterations/01-one.js
+++ 05-iterations/01-one.js
@@ -1,15 +1,31 @@
 // for
 
-for(let index = 0; index < 10; index++){
-    const element = index;
-    console.log(element);
-}
-for (let index = 0; index < 6; index++) {
-    for (let jndex = 0; jndex < 8; jndex++) {
-        const element = jndex;
-        console.log(element);
-    }
-    const element = index
-    console.log(element);
+// for(let index = 0; index < 10; index++){
+//     const element = index;
+//     console.log(element);
+// }
+// for (let index = 0; index < 6; index++) {
+//     for (let jndex = 0; jndex < 8; jndex++) {
+//         const element = jndex;
+//         console.log(element);
+//     }
+//     const element = index
+//     console.log(element);
     
+// }
+
+for (let index = 1; index <= 20; index++) {
+    if (index==5) {
+        console.log(`5 DETECTED`);
+        break;
+    }
+    console.log(`${index}`);  
+}
+
+for (let index = 1; index <= 20; index++) {
+    if (index==5) {
+        console.log(`5 DETECTED`);
+        continue;
+    }
+    console.log(`${index}`);  
 }
