6312c3802e2cbf10b33723e4073432993ac2eeed
Harshit Kumar <106590879+harshit38144@users.noreply.github.com>
2024-03-24 10:41:51 +0000

    for loop

---
+++ 05-iterations/01-one.js
@@ -0,0 +1,15 @@
+// for
+
+for(let index = 0; index < 10; index++){
+    const element = index;
+    console.log(element);
+}
+for (let index = 0; index < 6; index++) {
+    for (let jndex = 0; jndex < 8; jndex++) {
+        const element = jndex;
+        console.log(element);
+    }
+    const element = index
+    console.log(element);
+    
+}
