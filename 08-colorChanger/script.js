5d32de1705733fe2bf9802c93a155acd2be61b75
Harshit Kumar <106590879+harshit38144@users.noreply.github.com>
2024-04-06 11:10:29 +0000

    color changing

---
+++ 08-colorChanger/script.js
@@ -0,0 +1,30 @@
+const startbtn = document.getElementById('start')
+const stopbtn = document.getElementById('stop')
+
+const randomColor = function(){
+    const hex = "0123456789ABCDEF"
+    let color = '#'
+    for (let i = 0; i < 6; i++) {
+         color += hex[Math.floor(Math.random()*16)]
+    }
+    return color
+}
+
+let intervalId;
+
+function startChangingColor(){
+    if(!intervalId){
+        intervalId = setInterval(changeBgColor, 1000)
+    }
+
+    function changeBgColor(){
+        document.body.style.backgroundColor = randomColor()
+    }
+}
+function stopChangingColor(){
+    clearInterval(intervalId)
+    intervalId = null;
+}
+
+document.querySelector('#start').addEventListener('click', startChangingColor)
+document.querySelector('#stop').addEventListener('click', stopChangingColor)
